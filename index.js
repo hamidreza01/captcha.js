a2b273c50af4b8b38b19b34e04aa41a3de460eee
hamidreza ghahremani <97837767+hamidreza01@users.noreply.github.com>
2022-03-23 14:57:12 +0430

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,73 @@
 const sharp = require('sharp');
 const { rgb } = require('color');
 const generator = require('generate-password');
+const fs = require('fs');
+const random = (min, max) => {
+  return Math.floor(Math.random() * (min + max - 1) + 1);
+};
 
-module.exports = async ({ numbers, lowercase, uppercase, symbols }) => {
+const pCreator = (number) => {
+  let str = '';
+  for (let i = 0; i < number; i++) {
+    str += ` <path d="M${random(1, 50)} ${random(0, 50)}C${random(
+      10,
+      150
+    )} ${random(10, 90)},${random(150, 90)} ${random(10, 99)},${random(
+      150,
+      190
+    )} 9
+  " class="p1" stroke="rgb(${random(0, 256)},${random(0, 256)},${random(
+      0,
+      256
+    )})" ></path>`;
+  }
+  return str;
+};
+
+const lCreator = (number) => {
+  let str = '';
+  for (let i = 0; i < number; i++) {
+    str += `<line  stroke-width="${random(1, 5)}" y1="${random(
+      10,
+      30
+    )}" y2="${random(20, 80)}" x1="${random(10, 100)}" x2="${random(
+      30,
+      200
+    )}" class="l1" stroke="rgb(${random(0, 256)},${random(0, 256)},${random(
+      0,
+      256
+    )})"></line>`;
+  }
+  return str;
+};
+const tCreator = (text) => {
+  let str = '';
+  let x = 45;
+  for (let i = 0; i < text.length; i++) {
+    str += `<text id="text${i + 1}" x="${x}" 
+
+    transform="rotate(${random(0, 12)} ${random(0, 15)} ${random(0, 20)})"
+      y="55">${text[i]}</text>`;
+    x = x + 20;
+  }
+  return str;
+};
+
+module.exports = async ({
+  numbers,
+  lowercase,
+  uppercase,
+  symbols,
+  difficulty,
+  path,
+}) => {
   return new Promise(async (res, rej) => {
     try {
-      const random = (min, max) => {
-        return Math.floor(Math.random() * (min + max - 1) + 1);
-      };
+      fs.mkdir(path, (err) => {
+        if (err.code != 'EEXIST') {
+          console.log(err);
+        }
+      });
       const rgbs = rgb(random(0, 255), random(0, 255), random(0, 255));
       const text = generator.generate({
         length: 6,
@@ -18,10 +78,10 @@ module.exports = async ({ numbers, lowercase, uppercase, symbols }) => {
       });
       const file = `${generator.generate({
         length: 16,
-        numbers,
-        lowercase,
-        uppercase,
-        symbols,
+        numbers: false,
+        lowercase: true,
+        uppercase: true,
+        symbols: false,
       })}.png`;
       const width = 200;
       const height = 100;
@@ -51,6 +111,14 @@ font-size: 40px;
 fill : ${rgbs};
 stroke:rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)});
 stroke-width:${random(1, 3)}
+}length
+length
+length
+#text5 {
+font-size: 40px;
+fill : ${rgbs};
+stroke:rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)});
+stroke-width:${random(1, 3)}
 }
 #text5 {
 font-size: 40px;
@@ -59,11 +127,11 @@ stroke:rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)});
 stroke-width:${random(1, 3)}
 }
 #text6 {
-font-size: 40px;
-fill : ${rgbs};
-stroke:rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)});
-stroke-width:${random(1, 3)}
-}
+  font-size: 40px;
+  fill : ${rgbs};
+  stroke:rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)});
+  stroke-width:${random(1, 3)}
+  }
 .p1 {
 stroke-width: 5px;
 fill:none;
@@ -71,137 +139,11 @@ fill:none;
 }
 
 </style>
-  <path d="M${random(1, 50)} ${random(0, 50)}C${random(10, 150)} ${random(
-        10,
-        90
-      )},${random(150, 90)} ${random(10, 99)},${random(150, 190)} 9
-    " class="p1" stroke="rgb(${random(0, 256)},${random(0, 256)},${random(
-        0,
-        256
-      )})" ></path>
-    <path d="M${random(1, 50)} ${random(0, 50)}C${random(10, 150)} ${random(
-        10,
-        90
-      )},${random(150, 90)} ${random(10, 99)},${random(150, 190)} 9
-      " class="p1" stroke="rgb(${random(0, 256)},${random(0, 256)},${random(
-        0,
-        256
-      )})" ></path>
-    <line  stroke-width="${random(1, 5)}" y1="${random(10, 30)}" y2="${random(
-        20,
-        80
-      )}" x1="${random(10, 100)}" x2="${random(
-        30,
-        200
-      )}" class="l1" stroke="rgb(${random(0, 256)},${random(0, 256)},${random(
-        0,
-        256
-      )})"></line><line  stroke-width="${random(1, 5)}" y1="${random(
-        10,
-        30
-      )}" y2="${random(20, 80)}" x1="${random(10, 100)}" x2="${random(
-        30,
-        200
-      )}" class="l1" stroke="rgb(${random(0, 256)},${random(0, 256)},${random(
-        0,
-        256
-      )})"></line><line  stroke-width="${random(1, 5)}" y1="${random(
-        10,
-        30
-      )}" y2="${random(20, 80)}" x1="${random(10, 100)}" x2="${random(
-        30,
-        200
-      )}" class="l1" stroke="rgb(${random(0, 256)},${random(0, 256)},${random(
-        0,
-        256
-      )})"></line><line  stroke-width="${random(1, 5)}" y1="${random(
-        10,
-        30
-      )}" y2="${random(20, 80)}" x1="${random(10, 100)}" x2="${random(
-        30,
-        200
-      )}" class="l1" stroke="rgb(${random(0, 256)},${random(0, 256)},${random(
-        0,
-        256
-      )})"></line>
-    <line  stroke-width="${random(1, 5)}" y1="${random(10, 30)}" y2="${random(
-        20,
-        80
-      )}" x1="${random(10, 100)}" x2="${random(
-        30,
-        200
-      )}" class="l1" stroke="rgb(${random(0, 256)},${random(0, 256)},${random(
-        0,
-        256
-      )})"></line>
-    <line  stroke-width="${random(1, 5)}" y1="${random(10, 30)}" y2="${random(
-        20,
-        80
-      )}" x1="${random(10, 100)}" x2="${random(
-        30,
-        200
-      )}" class="l1" stroke="rgb(${random(0, 256)},${random(0, 256)},${random(
-        0,
-        256
-      )})"></line>
-    <line  stroke-width="${random(1, 5)}" y1="${random(10, 30)}" y2="${random(
-        20,
-        80
-      )}" x1="${random(10, 100)}" x2="${random(
-        30,
-        200
-      )}" class="l1" stroke="rgb(${random(0, 256)},${random(0, 256)},${random(
-        0,
-        256
-      )})"></line>
-    <line  stroke-width="${random(1, 5)}" y1="${random(10, 30)}" y2="${random(
-        20,
-        80
-      )}" x1="${random(10, 100)}" x2="${random(
-        30,
-        200
-      )}" class="l1" stroke="rgb(${random(0, 256)},${random(0, 256)},${random(
-        0,
-        256
-      )})"></line>
-    
-    <line  stroke-width="${random(1, 5)}" y1="${random(10, 30)}" y2="${random(
-        20,
-        80
-      )}" x1="${random(10, 100)}" x2="${random(
-        30,
-        200
-      )}" class="l1" stroke="rgb(${random(0, 256)},${random(0, 256)},${random(
-        0,
-        256
-      )})"></line>
-  <text id="text1" x="45" 
-  
-  transform="rotate(${random(0, 12)} ${random(0, 15)} ${random(0, 20)})"
-    y="55">${text[0]}</text>
-  <text id="text2" x="70" 
-  
-  transform="rotate(${random(0, 12)} ${random(0, 15)} ${random(0, 20)})"
-    y="55">${text[1]}</text>
-  <text id="text3" x="90" 
-  
-  transform="rotate(${random(0, 12)} ${random(0, 15)} ${random(0, 20)})"
-    y="55">${text[2]}</text>
-  <text id="text4" x="115" 
-  
-  transform="rotate(${random(0, 12)} ${random(0, 15)} ${random(0, 20)})"
-    y="55">${text[3]}</text>
-  <text id="text5" x="140" 
-  
-  transform="rotate(${random(0, 12)} ${random(0, 15)} ${random(0, 20)})"
-    y="55">${text[4]}</text>
-  <text id="text6" x="165" 
-  
-  transform="rotate(${random(0, 12)} ${random(0, 15)} ${random(0, 20)})"
-     y="55">${text[5]}</text>
+${pCreator(difficulty / 5 < 1 ? 2 : difficulty / 5)}
+${lCreator(difficulty * 5)}
+${tCreator(text)}
 </svg>
     `;
-      // <line y1="10" y2="90" x1="10" x2="190" id="l1"></line>
       const svgBuffer = Buffer.from(svgImage);
       const image = await sharp({
         create: {
@@ -218,9 +160,8 @@ fill:none;
             left: 0,
           },
         ])
-        .toFile(file)
+        .toFile(path + '/' + file)
         .then(() => {
-          console.log('im here');
           res({
             file_name: file,
             captcha: text,
